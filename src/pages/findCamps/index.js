24cabbc97b1d88a192ff6385739a02ac1cc0c5d7
Mounish Sai <pvsmounish@gmail.com>
2019-02-27 12:08:54 +0530

    Added Map to Find Relief Camps

--- src/pages/findCamps/index.js
+++ src/pages/findCamps/index.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import {
+    Icon, Card
+    } from 'antd';
+import GoogleMapReact from 'google-map-react';
+
+const MapMarker = ({ text }) => (
+    <Card style={{ width: 200, height: 70 }}>
+        <Icon type='pushpin' />
+        {text}
+    </Card>
+);
 
 export class FindCamps extends Component {
+
+    static defaultProps = {
+        center: {
+            lat: 12.975358,
+            lng: 79.162417
+        },
+        zoom: 11
+    };
     
     render() {
         return (
             <div>
                 <h2>Find Camps</h2>
+                <Card>
+                <div style={{ height: '100vh', width: '100%' }}>
+                    <GoogleMapReact
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    >
+                        <MapMarker
+                            lat={12.975358}
+                            lng={79.272417}
+                            text="Relief Camp"
+                        />
+                        <MapMarker
+                            lat={12.975358}
+                            lng={79.102417}
+                            text="Relief Camp"
+                        />
+                    </GoogleMapReact>
+                </div>
+                </Card>
             </div>
         );
     }
