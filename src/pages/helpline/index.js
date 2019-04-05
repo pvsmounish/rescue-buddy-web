0d57faecc211781b9978c39a4bd2335d7d7c00dd
Mounish Sai <pvsmounish@gmail.com>
2019-04-05 19:24:55 +0530

    :zap: Added Helpline Content

--- src/pages/helpline/index.js
+++ src/pages/helpline/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import {
+    Typography
+    } from 'antd';
+
+const { Text } = Typography;
 
 export class HelpLine extends Component {
     
     render() {
         return (
-            <div>
-                <h2>Help Line</h2>
+            <div style={{display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
+                <h2>Helpline</h2>
+                <Text>For Emergency Contact</Text>
+                <div style={{marginTop: 30}} />
+                <div style={{backgroundColor: '#ff0000', borderRadius: 10}}>
+                    <Text style={{fontSize: 40, color: '#fff'}}>+91-9000000000</Text>
+                </div>
             </div>
         );
     }
