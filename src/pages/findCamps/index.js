aa79c187b90bedd5a49ccf71d3d55a6202f896d0
Mounish Sai <pvsmounish@gmail.com>
2019-04-07 19:26:51 +0530

    :zap: Fetch Camps
    Added Query to fetch camps

--- src/pages/findCamps/index.js
+++ src/pages/findCamps/index.js
@@ -3,7 +3,21 @@ import {
     Icon, Card, Tooltip
     } from 'antd';
 import GoogleMapReact from 'google-map-react';
-import { googleMapApiKey } from '../../config'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { googleMapApiKey } from '../../config';
+
+const GET_RELIEFCAMPS = gql`
+    query reliefCamps{
+        reliefCamps{
+            campName,
+            latitude,
+            longitude,
+            address,
+            mobileNumber
+        }
+    }
+`;
 
 const MapMarker = ({ text }) => (
     <Tooltip title={text}>
@@ -45,22 +59,30 @@ export class FindCamps extends Component {
                 <h2>Find Camps</h2>
                 <Card>
                 <div style={{ height: '100vh', width: '100%' }}>
-                    <GoogleMapReact
-                    bootstrapURLKeys={{ key: googleMapApiKey }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                    >
-                        <MapMarker
-                            lat={12.975358}
-                            lng={79.272417}
-                            text="Relief Camp"
-                        />
-                        <MapMarker
-                            lat={12.975358}
-                            lng={79.102417}
-                            text="Relief Camp"
-                        />
-                    </GoogleMapReact>
+                    <Query query={GET_RELIEFCAMPS}>
+                        {({ loading, error, data }) => {
+                        if (loading) return <Icon type='loading' style={{fontSize: 40}}/>;
+                        if (error) return <p>Error :(</p>;
+                            
+                            return (
+                                <GoogleMapReact
+                                bootstrapURLKeys={{ key: googleMapApiKey }}
+                                defaultCenter={this.props.center}
+                                defaultZoom={this.props.zoom}
+                                >
+                                    {data.reliefCamps &&
+                                        data.reliefCamps.map(reliefCamp => (
+                                            <MapMarker
+                                                lat={reliefCamp.latitude}
+                                                lng={reliefCamp.longitude}
+                                                text={`${reliefCamp.campName} (${reliefCamp.mobileNumber}) - ${reliefCamp.address}`}
+                                            />
+                                    ))
+                                    }
+                                </GoogleMapReact>
+                            )
+                            }}
+                    </Query>
                 </div>
                 </Card>
             </div>
